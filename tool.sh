9f54fed2367c84abc51162841db42d99f0f105f9
John Kener <73270248+John-kener@users.noreply.github.com>
2020-12-05 20:26:34 +0530

    Create tool.sh

---
+++ tool.sh
@@ -0,0 +1,14 @@
+figlet -f mini INSTALLING
+figlet -f mini REQUIREMENTS 
+echo
+echo
+pkg install figlet -y
+gem install lolcat
+apt install toilet -y
+echo
+figlet -f mini DONE
+echo
+clear
+cd && cd Cyber-virus && cd base && bash intro.sh
+cd && cd Cyber-virus && cd base && bash tool.sh
+ 
