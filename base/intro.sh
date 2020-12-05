f29e415f23980eeca4f21c9408de87468209fe0f
John Kener <73270248+John-kener@users.noreply.github.com>
2020-12-05 20:30:37 +0530

    Create intro.sh

---
+++ base/intro.sh
@@ -0,0 +1,29 @@
+#colour codes
+
+r='\e[1;31m'
+g='\e[1;32m'
+y='\e[1;33m'
+b='\e[1;34m'
+p='\e[1;35m'
+lb='\e[1;36m'
+gr='\e[1;30m'
+wh='\e[1;37m'
+
+clear
+
+# LANCHING TOOL
+
+figlet -f banner CYBER |lolcat -a -d 5
+figlet -f banner WARRIORS |lolcat -a -d 5
+echo
+echo                                  ＢＹ  ＪＯＨＮ  ＫＥＮＥＲ |lolcat
+sleep 3
+clear
+echo
+echo
+echo -e $y"  TᕼIS TOOᒪ ᗯᗩS ᑕᖇEᗩTEᗪ ᗷY ᒍOᕼᑎ KEᑎEᖇ @ ᑕYᗷEᖇ ᗯᗩᖇᖇIOᖇS "
+echo
+echo -e $p"  ᗩᒪᒪ ᖇIGᕼTS ᖇESEᖇᐯEᗪ ᗷY ᒍOᕼᑎ KEᑎEᖇ "
+echo
+echo -e $r"  ᗪO ᑎOT ᑕOᑭYᖇIGᕼT Oᖇ ᗪEᑕᖇIᑭT TᕼIS ᖇEᑭOSITOᖇY "
+sleep 8
